1d2bdf90801cd7c7b891ed4c0927a1556a6ca75c
Kaique <kaique_ssantos@outlook.com>
2024-09-21 03:36:40 -0300

    infra k8s

--- main.tf
+++ main.tf
@@ -16,23 +16,7 @@ module "cluster-eks-fiap" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "cluster-eks-fiap"
   cluster_version = "1.23"
-  subnets         = [
-    "subnet-0af1a2b0c4e87a8de",
-    "subnet-09bbc7703c198652c",
-    "subnet-0e7c53c46d04ec3e5",
-    "subnet-052032e23566308d6",
-    "subnet-0121249a88ef4bc0a",
-    "subnet-09da51e5d063c8baf"
-  ]
   vpc_id          = "vpc-048be7ca8808d1272"
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_type    = "t3.medium"
-    }
-  }
 }
 
 output "cluster_endpoint" {
