1f6d8f3cb38fa4cb6d08b78ed9812a784d5689c4
Kaique <kaique_ssantos@outlook.com>
2024-09-26 16:28:26 -0300

    eks

--- lambdas.tf
+++ lambdas.tf
@@ -8,7 +8,6 @@ resource "aws_security_group" "lambda_sg" {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
-    # TODO: Allow API-GATEWAY
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -32,6 +31,7 @@ resource "aws_lambda_function" "valida_cpf_usuario" {
   runtime       = "nodejs18.x"
   s3_bucket     = var.bucket_food_lambdas
   s3_key        = "valida_cpf_usuario.zip"
+
   vpc_config {
     subnet_ids = [
       aws_subnet.food_private_subnet_1.id,
@@ -43,16 +43,6 @@ resource "aws_lambda_function" "valida_cpf_usuario" {
     ]
   }
 
-  # environment {
-  #   variables = {
-  #     DB_HOST     = aws_db_instance.food_database.address
-  #     DB_USER     = var.db_username
-  #     DB_PASSWORD = var.db_password
-  #     DB_NAME     = "fooddb"
-  #   }
-  # }
-
-  # Adicionando depends_on para garantir a ordem correta da dependência entre security groups
   depends_on = [
     aws_security_group.eks_security_group,
     aws_s3_object.valida_cpf_usuario
